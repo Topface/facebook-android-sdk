c7080d355cbc81d51c26d77ffc6980ac8fcb45bd
Andrey Mischenko <git@gildor.ru>
2012-12-06 19:14:33 +0400

    Fix package name in manifest

--- facebook/src/com/facebook/topface/FbDialog.java
+++ facebook/src/com/facebook/topface/FbDialog.java
@@ -35,7 +35,6 @@ import android.webkit.WebViewClient;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import com.facebook.android.R;
 import com.facebook.topface.Facebook.DialogListener;
 
 public class FbDialog extends Dialog {
